a41be63c106cf9c9e29fe01489599162319e0e0f
Tom Adams <tom@dxw.com>
2017-08-24 09:16:27 -0400

    Hook it up

--- hibp-check.php
+++ hibp-check.php
@@ -1,4 +1,12 @@
 <?php
+/*
+Plugin Name: HIBP Check
+Plugin URI: https://github.com/dxw/hibp-check
+Description: Checks passwords with haveibeenpwned.com, preventing users from using breached passwords
+Version: 0.1.0
+Author: dxw
+Author URI: https://www.dxw.com
+*/
 
 $registrar = require __DIR__.'/src/load.php';
 $registrar->register();
--- src/di.php
+++ src/di.php
@@ -1,1 +1,10 @@
 <?php
+
+$url = defined('HIBP_CHECK_URL') ? HIBP_CHECK_URL : null;
+
+$registrar->addInstance(new \HibpCheck\HibpApi($url));
+
+$registrar->addInstance(new \HibpCheck\PasswordChange(
+    $registrar->getInstance(\HibpCheck\HibpApi::class),
+    $registrar->getInstance(\Dxw\Iguana\Value\Post::class)
+));
